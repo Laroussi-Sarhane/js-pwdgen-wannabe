370b0ec0aa56a2d290192971794b5d676a1bca77
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-02-19 15:44:50 +0100

    add js script

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 console.log('ciao')
-const name = prompt('')
+const firstName = prompt('scrivi il tuo nome');
+
+const surname = prompt ('scrivi il tuo cognome');
+
+const bestColor = prompt (' scrivi il tuo cololore preferito')
+
+const fullName = firstName + surname + bestColor;
+
+console.log(firstName, surname, bestColor);
+
+document.getElementById('nomeutente').innerHTML =`
+<p> ciao  ${fullName}, </p>
+`;
+
